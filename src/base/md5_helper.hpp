36245df3f2c0b4d15ce180f25909b6852acf25fe
xqq <xqq@xqq.im>
2021-05-16 14:38:09 +0900

    base/md5_helper: Add MD5 helper

---
+++ src/base/md5_helper.hpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARIBCAPTION_MD5_HELPER_HPP
+#define ARIBCAPTION_MD5_HELPER_HPP
+
+#include <cstdint>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include "base/md5.h"
+
+namespace aribcaption {
+namespace md5 {
+
+static std::string GetDigest(const uint8_t* buffer, size_t length) {
+    const uint8_t* ptr = buffer;
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+
+    while (length) {
+        if (length > 64) {
+            MD5_Update(&ctx, ptr, 64);
+            length -= 64;
+            ptr += 64;
+        } else {
+            MD5_Update(&ctx, ptr, length);
+            length -= length;
+            ptr += length;
+        }
+    }
+
+    std::vector<uint8_t> digest(16, 0);
+    MD5_Final(&digest[0], &ctx);
+
+    std::string digest_str(32, '\0');
+    for (int i = 0; i < 16; i++) {
+        snprintf(&digest_str[i * 2], 16 * 2, "%02x", static_cast<uint32_t>(digest[i]));
+    }
+
+    return digest_str;
+}
+
+}  // namespace md5
+}  // namespace aribcaption
+
+#endif  // ARIBCAPTION_MD5_HELPER_HPP

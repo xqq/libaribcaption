5f99f0aed83d1cc6541da001ade388785a8c2bb9
xqq <xqq@xqq.im>
2021-05-17 11:42:16 +0900

    include/caption: Add definitions for Caption, CaptionRegion, CaptionChar

---
+++ include/caption.hpp
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARIBCAPTION_CAPTION_HPP
+#define ARIBCAPTION_CAPTION_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "drcs.hpp"
+
+namespace aribcaption {
+
+constexpr int64_t PTS_NOPTS = ((int64_t)UINT64_C(0x8000000000000000));
+constexpr int64_t DURATION_INDEFINITE = ((int64_t)UINT64_C(0x8000000000000000));
+
+using B24ColorRGBA = uint32_t;
+
+enum CharStyle : uint8_t {
+    kCharStyleDefault = 0,
+    kCharStyleBold = 1 << 0,
+    kCharStyleItalic = 1 << 1,
+    kCharStyleUnderline = 1 << 2,
+    kCharStyleStroke = 1 << 3
+};
+
+enum EnclosureStyle : uint8_t {
+    kEnclosureStyleNone = 0,
+    kEnclosureStyleTop = 1 << 0,
+    kEnclosureStyleRight = 1 << 1,
+    kEnclosureStyleBottom = 1 << 2,
+    kEnclosureStyleLeft = 1 << 3,
+    kEnclosureStyleDefault = kEnclosureStyleNone
+};
+
+enum CaptionType : uint8_t {
+    kCaptionTypeCaption = 0x80,
+    kCaptionTypeSuperimpose = 0x81,
+    kCaptionTypeDefault = kCaptionTypeCaption
+};
+
+enum CaptionCharType : uint8_t {
+    kCaptionCharTypeText = 0,
+    kCaptionCharTypeTextGaiji,
+    kCaptionCharTypeDRCS,
+    kCaptionCharTypeDRCSReplaced,
+    kCaptionCharTypeDRCSReplacedGaiji,
+    kCaptionCharTypeDefault = kCaptionCharTypeText
+};
+
+struct CaptionChar {
+    CaptionCharType type = CaptionCharType::kCaptionCharTypeDefault;
+
+    std::string ch;
+    uint32_t drcs_id = 0;
+
+    int x = 0;
+    int y = 0;
+    int char_width = 0;
+    int char_height = 0;
+    int char_horizontal_spacing = 0;
+    int char_vertical_spacing = 0;
+    float char_horizontal_scale = 0.0f;
+    float char_vertical_scale = 0.0f;
+
+    B24ColorRGBA text_color = 0;
+    B24ColorRGBA back_color = 0;
+    B24ColorRGBA stroke_color = 0;
+
+    CharStyle style = CharStyle::kCharStyleDefault;
+    EnclosureStyle enclosure_style = EnclosureStyle::kEnclosureStyleDefault;
+public:
+    CaptionChar() = default;
+    CaptionChar(const CaptionChar&) = default;
+    CaptionChar(CaptionChar&&) = default;
+    CaptionChar& operator=(const CaptionChar&) = default;
+    CaptionChar& operator=(CaptionChar&&) = default;
+};
+
+struct CaptionRegion {
+    std::vector<CaptionChar> chars;
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
+    bool is_ruby = false;
+public:
+    CaptionRegion() = default;
+    CaptionRegion(const CaptionRegion&) = default;
+    CaptionRegion(CaptionRegion&&) = default;
+    CaptionRegion& operator=(const CaptionRegion&) = default;
+    CaptionRegion& operator=(CaptionRegion&&) = default;
+};
+
+struct Caption {
+    CaptionType type = CaptionType::kCaptionTypeDefault;
+
+    std::string text;
+    std::vector<CaptionRegion> regions;
+    std::unordered_map<uint32_t, DRCS> drcs_map;
+
+    int64_t pts = 0;
+    int64_t duration = 0;
+    int plane_width = 0;
+    int plane_height = 0;
+
+    bool has_builtin_sound = false;
+    uint8_t builtin_sound_id = 0;
+public:
+    Caption() = default;
+    Caption(const Caption&) = default;
+    Caption(Caption&&) = default;
+    Caption& operator=(const Caption&) = default;
+    Caption& operator=(Caption&&) = default;
+};
+
+}  // namespace aribcaption
+
+#endif  // ARIBCAPTION_CAPTION_HPP

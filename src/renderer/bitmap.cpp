090c5a684a6ff3db2982d442b2eb340f0951c426
xqq <xqq@xqq.im>
2021-09-25 14:57:25 +0900

    renderer/bitmap: Add class Bitmap implementation

---
+++ src/renderer/bitmap.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <cassert>
+#include "renderer/bitmap.hpp"
+
+namespace aribcaption {
+
+Image Bitmap::ToImage(Bitmap&& bmp) {
+    Image image;
+
+    image.width = bmp.width();
+    image.height = bmp.height();
+    image.stride = bmp.stride();
+    image.pixel_format = bmp.pixel_format();
+    image.bitmap = std::move(bmp.pixels);
+
+    bmp.width_ = 0;
+    bmp.height_ = 0;
+    bmp.stride_ = 0;
+
+    return image;
+}
+
+Bitmap::Bitmap(int width, int height, PixelFormat pixel_format) :
+      width_(width), height_(height), pixel_format_(pixel_format) {
+    assert(width > 0 && height > 0);
+    assert(pixel_format == kPixelFormatRGBA8888);
+
+    stride_ = width;
+
+    uint32_t padding = kAlignedTo - (width % kAlignedTo);
+    if (padding) {
+        stride_ += static_cast<int>(padding);
+    }
+
+    pixels.resize(stride_ * height * 4);
+}
+
+}  // namespace aribcaption
---
+++ src/renderer/bitmap.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARIBCAPTION_BITMAP_HPP
+#define ARIBCAPTION_BITMAP_HPP
+
+#include <cstdint>
+#include <vector>
+#include <type_traits>
+#include "aligned_alloc.hpp"
+#include "color.hpp"
+#include "image.hpp"
+
+namespace aribcaption {
+
+class Bitmap {
+public:
+    static constexpr size_t kAlignedTo = 32;
+public:
+    static Image ToImage(Bitmap&&);
+public:
+    Bitmap(int width, int height, PixelFormat pixel_format);
+    Bitmap(const Bitmap& bmp) = default;
+    Bitmap(Bitmap&& bmp) = default;
+    ~Bitmap() = default;
+public:
+    [[nodiscard]] inline uint8_t* data() { return pixels.data(); }
+
+    [[nodiscard]] inline const uint8_t* data() const { return pixels.data(); }
+
+    [[nodiscard]] inline ColorRGBA* GetPixels() {
+        return reinterpret_cast<ColorRGBA*>(pixels.data());
+    };
+
+    [[nodiscard]] inline const ColorRGBA* GetPixels() const {
+        return reinterpret_cast<const ColorRGBA*>(pixels.data());
+    };
+
+    [[nodiscard]] inline ColorRGBA* GetPixelAt(int x, int y) {
+        auto ptr = reinterpret_cast<ColorRGBA*>(pixels.data());
+        return ptr + y * stride_ + x;
+    };
+
+    [[nodiscard]] inline const ColorRGBA* GetPixelAt(int x, int y) const {
+        auto ptr = reinterpret_cast<const ColorRGBA*>(pixels.data());
+        return ptr + y * stride_ + x;
+    };
+
+    [[nodiscard]] inline size_t size() const { return pixels.size(); }
+
+    [[nodiscard]] inline int width() const { return width_; }
+
+    [[nodiscard]] inline int height() const { return height_; }
+
+    [[nodiscard]] inline int stride() const { return stride_; }
+
+    [[nodiscard]] inline PixelFormat pixel_format() const { return pixel_format_; }
+private:
+    int width_ = 0;
+    int height_ = 0;
+    int stride_ = 0;
+    PixelFormat pixel_format_ = PixelFormat::kPixelFormatDefault;
+
+    std::vector<uint8_t, AlignedAllocator<uint8_t, kAlignedTo>> pixels;
+};
+
+}  // namespace aribcaption
+
+#endif  // ARIBCAPTION_BITMAP_HPP

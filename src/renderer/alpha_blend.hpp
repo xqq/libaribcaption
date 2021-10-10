ab15e025566caec0f342c68ee629fc6e4883820f
xqq <xqq@xqq.im>
2021-10-10 20:39:54 +0900

    renderer/alpha_blend: Fix incorrect alpha blending formula

--- src/renderer/alpha_blend.hpp
+++ src/renderer/alpha_blend.hpp
@@ -34,17 +34,20 @@ ALWAYS_INLINE uint32_t Div255(uint32_t x) {
 ALWAYS_INLINE ColorRGBA BlendColor(ColorRGBA bg_color, ColorRGBA fg_color) {
     ColorRGBA color;
 
-    // Calculate blended RGB channel
-    // alpha is within range [0, 1] in the formula
-    // out_rgb = foreground_rgb * foreground_alpha + background_rgb * (1 - foreground_alpha)
-    color.r = Div255((uint32_t)fg_color.r * fg_color.a) + Div255((uint32_t)bg_color.r * (255 - fg_color.a));
-    color.g = Div255((uint32_t)fg_color.g * fg_color.a) + Div255((uint32_t)bg_color.g * (255 - fg_color.a));
-    color.b = Div255((uint32_t)fg_color.b * fg_color.a) + Div255((uint32_t)bg_color.b * (255 - fg_color.a));
-
     // Calculate blended alpha channel
     // alpha is within range [0, 1] in the formula
     // out_alpha = foreground_alpha + background_alpha * (1 - foreground_alpha)
     color.a = (uint32_t)fg_color.a + Div255((uint32_t)bg_color.a * (255 - fg_color.a));
+
+    // Calculate blended RGB channel
+    // alpha is within range [0, 1] in the formula
+    // out_rgb = (foreground_rgb * foreground_alpha + background_rgb * (1 - foreground_alpha)) / out_alpha
+    if (color.a) {
+        color.r = ((uint32_t)fg_color.r * fg_color.a + (uint32_t)bg_color.r * (255 - fg_color.a)) / color.a;
+        color.g = ((uint32_t)fg_color.g * fg_color.a + (uint32_t)bg_color.g * (255 - fg_color.a)) / color.a;
+        color.b = ((uint32_t)fg_color.b * fg_color.a + (uint32_t)bg_color.b * (255 - fg_color.a)) / color.a;
+    }
+    // else if alpha is 0, then RGB is 0
 
     return color;
 }

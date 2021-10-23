225c8eb892be0375959b71f72bfc46c618493f05
xqq <xqq@xqq.im>
2021-10-23 18:02:15 +0900

    renderer/region_renderer: Add RegionRenderer class implementation

---
+++ src/renderer/region_renderer.cpp
@@ -0,0 +1,245 @@
+/*
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <cassert>
+#include "renderer/bitmap.hpp"
+#include "renderer/canvas.hpp"
+#include "renderer/region_renderer.hpp"
+
+namespace aribcaption {
+
+RegionRenderer::RegionRenderer(Context& context) : context_(context), log_(GetContextLogger(context)) {}
+
+bool RegionRenderer::Initialize(FontProviderType font_provider_type, TextRendererType text_renderer_type) {
+    font_provider_ = FontProvider::Create(font_provider_type, context_);
+    if (!font_provider_->Initialize()) {
+        return false;
+    }
+
+    text_renderer_ = TextRenderer::Create(text_renderer_type, context_, *font_provider_);
+    if (text_renderer_->Initialize()) {
+        return false;
+    }
+
+    return true;
+}
+
+bool RegionRenderer::SetFontFamily(const std::vector<std::string>& font_family) {
+    assert(text_renderer_);
+    return text_renderer_->SetFontFamily(font_family);
+}
+
+void RegionRenderer::SetOriginPlaneSize(int plane_width, int plane_height) {
+    assert(plane_width > 0 && plane_height > 0);
+    plane_width_ = plane_width;
+    plane_height_ = plane_height;
+    plane_inited_ = true;
+
+    if (caption_area_inited_) {
+        x_magnification_ = static_cast<float>(caption_area_width_) / static_cast<float>(plane_width);
+        y_magnification_ = static_cast<float>(caption_area_height_) / static_cast<float>(plane_height);
+    }
+}
+
+void RegionRenderer::SetTargetCaptionAreaRect(const Rect& rect) {
+    caption_area_start_x_ = rect.left;
+    caption_area_start_y_ = rect.top;
+    caption_area_width_ = rect.width();
+    caption_area_height_ = rect.height();
+    caption_area_inited_ = true;
+
+    if (plane_inited_) {
+        x_magnification_ = static_cast<float>(caption_area_width_) / static_cast<float>(plane_width_);
+        y_magnification_ = static_cast<float>(caption_area_height_) / static_cast<float>(plane_height_);
+    }
+}
+
+void RegionRenderer::SetReplaceDRCS(bool replace) {
+    replace_drcs_ = replace;
+}
+
+void RegionRenderer::SetForceStrokeText(bool force_stroke) {
+    force_stroke_text_ = force_stroke;
+}
+
+void RegionRenderer::SetForceNoBackground(bool force_no_background) {
+    force_no_background_ = force_no_background;
+}
+
+auto RegionRenderer::RenderCaptionRegion(const CaptionRegion& region,
+                                         const std::unordered_map<uint32_t, DRCS>& drcs_map)
+                                         -> Result<Image, RegionRenderError> {
+    assert(text_renderer_ && plane_inited_ && caption_area_inited_);
+
+    size_t char_count = region.chars.size();
+    size_t succeed = 0;
+    bool has_font_not_found_error = false;
+    bool has_codepoint_not_found_error = false;
+    bool has_other_error = false;
+
+    Bitmap bitmap(ScaleWidth(region.width),
+                  ScaleHeight(region.height),
+                  PixelFormat::kRGBA8888);
+    Canvas canvas(bitmap);
+    canvas.SetTextRenderer(*text_renderer_);
+
+    for (const CaptionChar& ch : region.chars) {
+        int section_x = ScaleX(ch.x - region.x);
+        int section_y = ScaleY(ch.y - region.y);
+        Rect section_rect(section_x,
+                          section_y,
+                          section_x + ScaleWidth(ch.section_width()),
+                          section_y + ScaleHeight(ch.section_height()));
+        // Draw background if not disabled
+        if (!force_no_background_) {
+            canvas.ClearRect(ch.back_color, section_rect);
+        }
+
+        // Draw enclosure if needed
+        if (ch.enclosure_style) {
+            int w = ScaleX(1);  // use floor
+            int h = ScaleY(1);  // use floor
+            if (ch.enclosure_style & EnclosureStyle::kEnclosureStyleTop) {
+                canvas.ClearRect(ch.text_color,
+                                 Rect(section_rect.left,
+                                      section_rect.top,
+                                      section_rect.right,
+                                      section_rect.top + h));
+            }
+            if (ch.enclosure_style & EnclosureStyle::kEnclosureStyleBottom) {
+                canvas.ClearRect(ch.text_color,
+                                 Rect(section_rect.left,
+                                      section_rect.bottom - h,
+                                      section_rect.right,
+                                      section_rect.bottom));
+            }
+            if (ch.enclosure_style & EnclosureStyle::kEnclosureStyleLeft) {
+                canvas.ClearRect(ch.text_color,
+                                 Rect(section_rect.left,
+                                      section_rect.top,
+                                      section_rect.left + w,
+                                      section_rect.bottom));
+            }
+            if (ch.enclosure_style & EnclosureStyle::kEnclosureStyleRight) {
+                canvas.ClearRect(ch.text_color,
+                                 Rect(section_rect.right - w,
+                                      section_rect.top,
+                                      section_rect.right,
+                                      section_rect.bottom));
+            }
+        }
+
+        int char_x = ScaleX((float)(ch.x - region.x) + (float)ch.char_horizontal_spacing * ch.char_horizontal_scale / 2);
+        int char_y = ScaleY((float)(ch.y - region.y) + (float)ch.char_vertical_spacing * ch.char_vertical_scale / 2);
+        int char_width = ScaleWidth((float)ch.char_width * ch.char_horizontal_scale);
+        int char_height = ScaleHeight((float)ch.char_height * ch.char_vertical_scale);
+
+        CaptionCharType type = ch.type;
+        CharStyle style = ch.style;
+        ColorRGBA stroke_color = ch.stroke_color;
+        int stroke_width = ScaleX(1.5f);  // use floor
+        UnderlineInfo underline_info{section_rect.left, section_rect.width()};
+
+        if (force_stroke_text_ && !(ch.style & CharStyle::kCharStyleStroke)) {
+            style = static_cast<CharStyle>(ch.style | CharStyle::kCharStyleStroke);
+            // Use background color for stroke text when forcing stroke text
+            stroke_color = ch.back_color;
+        }
+
+        // Draw char
+        if (type == CaptionCharType::kText || type == CaptionCharType::kGaiji) {
+            TextRenderStatus status = canvas.DrawChar(ch.ucs4, style, ch.text_color, stroke_color, stroke_width,
+                                                      char_width, char_height, char_x, char_y, underline_info);
+            if (status == TextRenderStatus::kOK) {
+                succeed++;
+            } else {
+                log_->e("RegionRenderer: canvas.DrawChar() returned error: ", static_cast<int>(status));
+                if (status == TextRenderStatus::kFontNotFound) {
+                    has_font_not_found_error = true;
+                } else if (status == TextRenderStatus::kCodePointNotFound) {
+                    has_codepoint_not_found_error = true;
+                } else if (status == TextRenderStatus::kOtherError) {
+                    has_other_error = true;
+                }
+            }
+        } else if ((replace_drcs_ && type == CaptionCharType::kDRCSReplaced) ||
+                   (replace_drcs_ && type == CaptionCharType::kDRCSReplacedGaiji)) {
+            // Draw replaced DRCS (alternative ucs4)
+            TextRenderStatus status = canvas.DrawChar(ch.ucs4, style, ch.text_color, stroke_color, stroke_width,
+                                                      char_width, char_height, char_x, char_y, underline_info);
+            if (status == TextRenderStatus::kOK) {
+                succeed++;
+            } else {
+                if (status == TextRenderStatus::kCodePointNotFound) {
+                    log_->w("RegionRenderer: Cannot find alternative codepoint U+", std::hex, ch.ucs4,
+                            ", fallback to DRCS rendering");
+                    has_codepoint_not_found_error = true;
+                } else {
+                    log_->e("RegionRenderer: canvas.DrawChar() returned error: ", static_cast<int>(status));
+                    if (status == TextRenderStatus::kFontNotFound) {
+                        has_font_not_found_error = true;
+                    } else if (status == TextRenderStatus::kOtherError) {
+                        has_other_error = true;
+                    }
+                }
+                // Fallback to DRCS rendering
+                type = CaptionCharType::kDRCS;
+            }
+        } else if (!replace_drcs_) {
+            // if DRCS replacement is disabled, force fallback to DRCS rendering
+            type = CaptionCharType::kDRCS;
+        }
+
+        // Draw DRCS
+        if (type == CaptionCharType::kDRCS) {
+            auto iter = drcs_map.find(ch.drcs_id);
+            if (iter != drcs_map.end()) {
+                const DRCS& drcs = iter->second;
+                bool ret = drcs_renderer_.DrawDRCS(drcs, style, ch.text_color, stroke_color, stroke_width,
+                                                   char_width, char_height, bitmap, char_x, char_y);
+                if (ret) {
+                    succeed++;
+                } else {
+                    log_->e("RegionRenderer: drcs_renderer_.DrawDRCS() returned error");
+                }
+            } else {
+                // DRCS not found in drcs_map
+                log_->e("RegionRenderer: Missing DRCS for drcs_id ", ch.drcs_id);
+            }
+        }
+    }
+
+    // If there's no successfully rendered char, return RegionRenderError
+    if (char_count > 0 && succeed == 0) {
+        if (has_font_not_found_error) {
+            return Err(RegionRenderError::kFontNotFound);
+        } else if (has_codepoint_not_found_error) {
+            return Err(RegionRenderError::kCodePointNotFound);
+        } else {
+            return Err(RegionRenderError::kOtherError);
+        }
+    }
+
+    Image image = Bitmap::ToImage(std::move(bitmap));
+    image.dst_x = caption_area_start_x_ + ScaleX(region.x);
+    image.dst_y = caption_area_start_y_ + ScaleY(region.y);
+
+    return Ok(std::move(image));
+}
+
+}  // namespace aribcaption
---
+++ src/renderer/region_renderer.hpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARIBCAPTION_REGION_RENDERER_HPP
+#define ARIBCAPTION_REGION_RENDERER_HPP
+
+#include <cmath>
+#include <vector>
+#include <string>
+#include <memory>
+#include <unordered_map>
+#include "caption.hpp"
+#include "context.hpp"
+#include "image.hpp"
+#include "base/logger.hpp"
+#include "base/result.hpp"
+#include "renderer/drcs_renderer.hpp"
+#include "renderer/font_provider.hpp"
+#include "renderer/rect.hpp"
+#include "renderer/text_renderer.hpp"
+
+namespace aribcaption {
+
+enum class RegionRenderError {
+    kFontNotFound,
+    kCodePointNotFound,
+    kOtherError,
+};
+
+class RegionRenderer {
+public:
+    explicit RegionRenderer(Context& context);
+    ~RegionRenderer() = default;
+public:
+    bool Initialize(FontProviderType font_provider_type = FontProviderType::kAuto,
+                    TextRendererType text_renderer_type = TextRendererType::kAuto);
+    // void SetFontLanguage();  // TODO
+    bool SetFontFamily(const std::vector<std::string>& font_family);
+    void SetOriginPlaneSize(int plane_width, int plane_height);
+    void SetTargetCaptionAreaRect(const Rect& rect);
+    void SetReplaceDRCS(bool replace);
+    void SetForceStrokeText(bool force_stroke);
+    void SetForceNoBackground(bool force_no_background);
+    auto RenderCaptionRegion(const CaptionRegion& region,
+                             const std::unordered_map<uint32_t, DRCS>& drcs_map) -> Result<Image, RegionRenderError>;
+private:
+    template <typename T>
+    [[nodiscard]]
+    int ScaleX(T x) const {
+        return static_cast<int>(std::floor(static_cast<float>(x) * x_magnification_));
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    int ScaleY(T y) const {
+        return static_cast<int>(std::floor(static_cast<float>(y) * y_magnification_));
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    int ScaleWidth(T width) const {
+        return static_cast<int>(std::ceil(static_cast<float>(width) * x_magnification_));
+    }
+
+    template <typename T>
+    [[nodiscard]]
+    int ScaleHeight(T height) const {
+        return static_cast<int>(std::ceil(static_cast<float>(height) * y_magnification_));
+    }
+public:
+    RegionRenderer(const RegionRenderer&) = delete;
+    RegionRenderer& operator=(const RegionRenderer&) = delete;
+private:
+    Context& context_;
+    std::shared_ptr<Logger> log_;
+
+    std::unique_ptr<FontProvider> font_provider_;
+    std::unique_ptr<TextRenderer> text_renderer_;
+    DRCSRenderer drcs_renderer_;
+
+    bool plane_inited_ = false;
+    int plane_width_ = 0;
+    int plane_height_ = 0;
+
+    bool caption_area_inited_ = false;
+    int caption_area_start_x_ = 0;
+    int caption_area_start_y_ = 0;
+    int caption_area_width_ = 0;
+    int caption_area_height_ = 0;
+
+    bool replace_drcs_ = true;
+    bool force_stroke_text_ = true;
+    bool force_no_background_ = false;
+
+    float x_magnification_ = 0.0f;
+    float y_magnification_ = 0.0f;
+};
+
+}  // namespace aribcaption
+
+#endif  // ARIBCAPTION_REGION_RENDERER_HPP

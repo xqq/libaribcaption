f7aa8692a459b7840f2c1817f20d86d284dd5508
xqq <xqq@xqq.im>
2021-12-25 22:51:01 +0900

    renderer/text_renderer_freetype: Fix incorrect alpha calculate in FTBitmapToColoredBitmap()

--- src/renderer/text_renderer_freetype.cpp
+++ src/renderer/text_renderer_freetype.cpp
@@ -246,9 +246,9 @@ Bitmap TextRendererFreetype::FTBitmapToColoredBitmap(const FT_Bitmap& ft_bmp, Co
         ColorRGBA* dest = bitmap.GetPixelAt(0, static_cast<int>(y));
 
         for (uint32_t x = 0; x < ft_bmp.width; x++) {
-            uint8_t alpha = src[x];
-            ColorRGBA rgb = alpha == 0 ? ColorRGBA() : color;
-            dest[x] = ColorRGBA(rgb, alpha);
+            uint8_t grey = src[x];
+            uint8_t alpha = alphablend::Div255(static_cast<uint32_t>(color.a) * grey);
+            dest[x] = ColorRGBA(color, alpha);
         }
     }
 

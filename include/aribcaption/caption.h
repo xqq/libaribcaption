87d167df1e9fd42190db128d8ab56b56aac8a450
xqq <xqq@xqq.im>
2022-01-28 21:06:45 +0900

    aribcaption/caption: Add documents for headers

--- include/aribcaption/caption.h
+++ include/aribcaption/caption.h
@@ -29,12 +29,32 @@
 extern "C" {
 #endif
 
-// For encoding ISO 639-2 3-char language codes
+/**
+ * Helper macro for encoding ISO 639-2 3-char language code
+ *
+ * @param a,b,c char
+ * @code
+ * uint32_t language_code = ARIBCC_MAKE_LANG('j', 'p', 'n')
+ * @endcode
+ */
 #define ARIBCC_MAKE_LANG(a,b,c) ((((a) & 0xff) << 16) | (((b) & 0xff) << 8) | ((c) & 0xff))
 
+/**
+ * Constant for marking the PTS is undefined.
+ */
 const int64_t ARIBCC_PTS_NOPTS = (int64_t)UINT64_C(0x8000000000000000);
+
+/**
+ * Constant for marking the duration is indefinite.
+ *
+ * Some ARIB captions have indefinite duration which means the caption's end time is undetermined.
+ * Captions with indefinite duration should be presented until the next caption's PTS.
+ */
 const int64_t ARIBCC_DURATION_INDEFINITE = (int64_t)UINT64_C(0x7FFFFFFFFFFFFFFF);
 
+/**
+ * Per-CaptionChar character styles
+ */
 typedef enum aribcc_charstyle_t {
     ARIBCC_CHARSTYLE_DEFAULT = 0,
     ARIBCC_CHARSTYLE_BOLD = 1u << 0,
@@ -43,6 +63,9 @@ typedef enum aribcc_charstyle_t {
     ARIBCC_CHARSTYLE_STROKE = 1u << 3
 } aribcc_charstyle_t;
 
+/**
+ * Per-CaptionChar enclosure styles
+ */
 typedef enum aribcc_enclosurestyle_t {
     ARIBCC_ENCLOSURESTYLE_NONE = 0,
     ARIBCC_ENCLOSURESTYLE_TOP = 1u << 0,
@@ -52,12 +75,22 @@ typedef enum aribcc_enclosurestyle_t {
     ARIBCC_ENCLOSURESTYLE_DEFAULT = ARIBCC_ENCLOSURESTYLE_NONE
 } aribcc_enclosurestyle_t;
 
+/**
+ * Enums for caption type indication. Usually, kCaption should be used.
+ *
+ * kSuperimpose should be indicated if you are handling ARIB superimpose (a kind of emergency caption).
+ */
 typedef enum aribcc_captiontype_t {
     ARIBCC_CAPTIONTYPE_CAPTION = 0x80,
     ARIBCC_CAPTIONTYPE_SUPERIMPOSE = 0x81,
     ARIBCC_CAPTIONTYPE_DEFAULT = ARIBCC_CAPTIONTYPE_CAPTION
 } aribcc_captiontype_t;
 
+/**
+ * Represents the type of CaptionChar.
+ *
+ * The type will be kDRCS or kDRCSReplaced if it's a DRCS character.
+ */
 typedef enum aribcc_chartype_t {
     ARIBCC_CHARTYPE_TEXT = 0,
     ARIBCC_CHARTYPE_DRCS = 1,
@@ -65,14 +98,34 @@ typedef enum aribcc_chartype_t {
     ARIBCC_CHARTYPE_DEFAULT = ARIBCC_CHARTYPE_TEXT
 } aribcc_chartype_t;
 
+/**
+ * Represents a caption character.
+ */
 typedef struct aribcc_caption_char_t {
     aribcc_chartype_t type;
 
-    // Character's Unicode codepoint (UCS4)
+    /**
+     * Character's Unicode codepoint (UCS4). This field will be 0 if CaptionCharType is kDRCS.
+     */
     uint32_t codepoint;
-    // Character's PUA(Private Use Area) codepoint, if exists
+
+    /**
+     * Character's PUA(Private Use Area) codepoint (UCS4)
+     *
+     * This field will be non-zero only if PUA code is defined for the character.
+     *
+     * Some ARIB additional symbols (i.e. Gaiji) are mapped into Private Use Area in Unicode BMP.
+     * Though most of them could be mapped into characters introduced in Unicode 5.2,
+     * Some fonts don't support Unicode 5.2 but support ARIB additional symbols through Private Use Area codes.
+     */
     uint32_t pua_codepoint;
-    // DRCS character's code
+
+    /**
+     * DRCS character's code. Only appears if if CaptionCharType is kDRCS or kDRCSReplaced.
+     * It is a private code which is unrelated to Unicode.
+     *
+     * DRCS data could be retrieved from Caption::drcs_map using drcs_code.
+     */
     uint32_t drcs_code;
 
     int x;
@@ -84,20 +137,28 @@ typedef struct aribcc_caption_char_t {
     float char_horizontal_scale;
     float char_vertical_scale;
 
-    aribcc_color_t text_color;
-    aribcc_color_t back_color;
-    aribcc_color_t stroke_color;
+    aribcc_color_t text_color;    ///< Color of the text (foreground)
+    aribcc_color_t back_color;    ///< Color of the background
+    aribcc_color_t stroke_color;  ///< Color of the storke text
 
     aribcc_charstyle_t style;
     aribcc_enclosurestyle_t enclosure_style;
 
-    // Character encoded in UTF-8
+    /**
+     * String representation of character encoded in UTF-8. This string is Null-terminated.
+     * Will be empty string if CaptionCharType is kDRCS because alternative codepoint is unknown.
+     */
     char u8str[8];
 } aribcc_caption_char_t;
 
-
+/**
+ * Calculate the width of the character block
+ */
 ARIBCC_API int aribcc_caption_char_get_section_width(aribcc_caption_char_t* caption_char);
 
+/**
+ * Calculate the height of the character block
+ */
 ARIBCC_API int aribcc_caption_char_get_section_height(aribcc_caption_char_t* caption_char);
 
 
@@ -149,52 +210,138 @@ ARIBCC_API aribcc_drcs_t* aribcc_drcsmap_get(aribcc_drcsmap_t* drcs_map, uint32_
 ARIBCC_API void aribcc_drcsmap_clear(aribcc_drcsmap_t* drcs_map);
 
 
+/**
+ * Structure represents a caption region.
+ *
+ * Call @aribcc_caption_region_cleanup() if you need to release a region which is received from aribcc API.
+ */
 typedef struct aribcc_caption_region_t {
     int x;
     int y;
     int width;
     int height;
-    bool is_ruby;
+    bool is_ruby;           ///< Will be true if the region is likely to be ruby text (furigana)
 
+    /**
+     * Caption char array. Element count is indicated by char_count.
+     *
+     * Do not manually free this array if the region is received from aribcc API,
+     * instead, call @aribcc_caption_region_cleanup().
+     */
     aribcc_caption_char_t* chars;
     uint32_t char_count;
 } aribcc_caption_region_t;
 
-
+/**
+ * Caption region cleanup function.
+ *
+ * Call this function only if if you received the region from aribcc API.
+ * Otherwise it may cause a crash.
+ *
+ * This function doesn't release the memory of the region itself.
+ */
 ARIBCC_API void aribcc_caption_region_cleanup(aribcc_caption_region_t* region);
 
-
+/**
+ * Constants for flags contained in Caption.
+ */
 typedef enum aribcc_captionflags_t {
     ARIBCC_CAPTIONFLAGS_DEFAULT = 0,
-    ARIBCC_CAPTIONFLAGS_CLEARSCREEN = 1u << 0,
-    ARIBCC_CAPTIONFLAGS_WAITDURATION =  1u << 1
+    ARIBCC_CAPTIONFLAGS_CLEARSCREEN = 1u << 0,     ///< Screen should be cleared before the caption presentation
+    ARIBCC_CAPTIONFLAGS_WAITDURATION =  1u << 1    ///< The caption has a determined duration
 } aribcc_captionflags_t;
 
+/**
+ * Structure represents a caption.
+ *
+ * Call @aribcc_caption_cleanup() if you need to release a caption which is received from the decoder.
+ */
 typedef struct aribcc_caption_t {
     aribcc_captiontype_t type;
     aribcc_captionflags_t flags;
 
-    // ISO 639-2 3-char language code, in Big Endian
-    // e.g. "jpn" => 6A 70 6E => 0x006A706E
+    /**
+     * ISO 639-2 3-char language code
+     * e.g. "jpn" => 6A 70 6E => 0x006A706E
+     */
     uint32_t iso6392_language_code;
 
+    /**
+     * Caption statements represented in UTF-8 string.
+     * Ruby text is excluded in this string.
+     *
+     * Pay attention to the UTF-8 encoding if you are under Windows.
+     *
+     * Do not manually free this pointer if the caption is received from the decoder,
+     * instead, call @aribcc_caption_cleanup().
+     */
     char* text;
 
+    /**
+     * Caption region array. Element count is indicated by region_count.
+     *
+     * Do not manually free this array if the caption is received from the decoder,
+     * instead, call @aribcc_caption_cleanup().
+     */
     aribcc_caption_region_t* regions;
-    uint32_t region_count;
+    uint32_t region_count;             ///< element count of regions array
 
+    /**
+     * DRCS hashmap, may be NULL if DRCS not exists
+     *
+     * Do not manually free this pointer if the caption is received from the decoder,
+     * instead, call @aribcc_caption_cleanup().
+     */
     aribcc_drcsmap_t* drcs_map;
 
+    /**
+     * Caption't presentation timestamp, in milliseconds
+     *
+     * Will be @ARIBCC_PTS_NOPTS if passed as @ARIBCC_PTS_NOPTS into decoder, otherwise in milliseconds.
+     */
     int64_t pts;
+
+    /**
+     * Caption's duration, in milliseconds
+     *
+     * Will be @ARIBCC_DURATION_INDEFINITE if undetermined, otherwise in milliseconds.
+     */
     int64_t wait_duration;
+
+    /**
+     * Width of the original(logical) caption plane. Usually has a value of 960 or 720.
+     */
     int plane_width;
+
+    /**
+     * Height of the original(logical) caption plane. Usually has a value of 540 or 480.
+     */
     int plane_height;
 
+    /**
+     * Represents whether the caption indicates a Built-in Sound Replay.
+     */
     bool has_builtin_sound;
+
+    /**
+     * The ID of build-in sound for playback. Valid only if has_builtin_sound is true.
+     */
     uint8_t builtin_sound_id;
 } aribcc_caption_t;
 
 
+/**
+ * Release all the dynamic-allocated inner fields of a caption.
+ *
+ * If you received a caption from the decoder, you should always call this function for cleanup
+ * rather than manually free the pointers inside the struct, otherwise it may cause a crash.
+ *
+ * On the other hand, if you constructed an aribcc_caption_t structure on your own,
+ * you should always manually free the pointers inside which is allocated by yourself.
+ * Call this function on your self-allocated aribcc_caption_t structure may cause a crash.
+ *
+ * This function doesn't release the memory of the caption itself.
+ */
 ARIBCC_API void aribcc_caption_cleanup(aribcc_caption_t* caption);
 
 
--- include/aribcaption/caption.hpp
+++ include/aribcaption/caption.hpp
@@ -29,10 +29,25 @@
 
 namespace aribcaption {
 
+/**
+ * Constant for marking the PTS is undefined.
+ */
 constexpr int64_t PTS_NOPTS = ((int64_t)UINT64_C(0x8000000000000000));            // -1
+
+/**
+ * Constant for marking the duration is indefinite.
+ *
+ * Some ARIB captions have indefinite duration which means the caption's end time is undetermined.
+ * Captions with indefinite duration should be presented until the next caption's PTS.
+ */
 constexpr int64_t DURATION_INDEFINITE = ((int64_t)UINT64_C(0x7FFFFFFFFFFFFFFF));  // int64_t max value
 
-// For encoding ISO 639-2 3-char language codes
+/**
+ * Helper function for encoding ISO 639-2 3-char language code
+ *
+ * @param str 3-character string literal, e.g. "jpn"
+ * @return e.g. "jpn" => 6A 70 6E => 0x006A706E
+ */
 template <size_t N>
 inline constexpr uint32_t ThreeCC(const char (&str)[N]) {
     static_assert(N == 4, "ISO 639-2 literals must be exactly 3 characters long");
@@ -41,6 +56,9 @@ inline constexpr uint32_t ThreeCC(const char (&str)[N]) {
             (static_cast<uint32_t>(str[2]) <<  0);
 }
 
+/**
+ * Per-CaptionChar character styles
+ */
 enum CharStyle {
     kCharStyleDefault = 0,
     kCharStyleBold = 1u << 0,
@@ -49,6 +67,9 @@ enum CharStyle {
     kCharStyleStroke = 1u << 3
 };
 
+/**
+ * Per-CaptionChar enclosure styles
+ */
 enum EnclosureStyle {
     kEnclosureStyleNone = 0,
     kEnclosureStyleTop = 1u << 0,
@@ -58,27 +79,58 @@ enum EnclosureStyle {
     kEnclosureStyleDefault = kEnclosureStyleNone
 };
 
+/**
+ * Enums for caption type indication. Usually, kCaption should be used.
+ *
+ * kSuperimpose should be indicated if you are handling ARIB superimpose (a kind of emergency caption).
+ */
 enum class CaptionType : uint8_t {
     kCaption = 0x80,
     kSuperimpose = 0x81,
     kDefault = kCaption
 };
 
+
+/**
+ * Represents the type of CaptionChar.
+ *
+ * The type will be kDRCS or kDRCSReplaced if it's a DRCS character.
+ */
 enum class CaptionCharType {
     kText = 0,
     kDRCS,
-    kDRCSReplaced,
+    kDRCSReplaced,    ///< DRCS has been replaced into Unicode codepoint
     kDefault = kText
 };
 
+/**
+ * Represents a caption character.
+ */
 struct CaptionChar {
     CaptionCharType type = CaptionCharType::kDefault;
 
-    // Character's Unicode codepoint (UCS4)
+    /**
+     * Character's Unicode codepoint (UCS4). This field will be 0 if CaptionCharType is kDRCS.
+     */
     uint32_t codepoint = 0;
-    // Character's PUA(Private Use Area) codepoint, if exists
+
+    /**
+     * Character's PUA(Private Use Area) codepoint (UCS4)
+     *
+     * This field will be non-zero only if PUA code is defined for the character.
+     *
+     * Some ARIB additional symbols (i.e. Gaiji) are mapped into Private Use Area in Unicode BMP.
+     * Though most of them could be mapped into characters introduced in Unicode 5.2,
+     * Some fonts don't support Unicode 5.2 but support ARIB additional symbols through Private Use Area codes.
+     */
     uint32_t pua_codepoint = 0;
-    // DRCS character's code
+
+    /**
+     * DRCS character's code. Only appears if if CaptionCharType is kDRCS or kDRCSReplaced.
+     * It is a private code which is unrelated to Unicode.
+     *
+     * DRCS data could be retrieved from Caption::drcs_map using drcs_code.
+     */
     uint32_t drcs_code = 0;
 
     int x = 0;
@@ -90,38 +142,50 @@ struct CaptionChar {
     float char_horizontal_scale = 0.0f;
     float char_vertical_scale = 0.0f;
 
-    ColorRGBA text_color;
-    ColorRGBA back_color;
-    ColorRGBA stroke_color;
+    ColorRGBA text_color;    ///< Color of the text (foreground)
+    ColorRGBA back_color;    ///< Color of the background
+    ColorRGBA stroke_color;  ///< Color of the storke text
 
     CharStyle style = CharStyle::kCharStyleDefault;
     EnclosureStyle enclosure_style = EnclosureStyle::kEnclosureStyleDefault;
 
-    // Character encoded in UTF-8
+    /**
+     * String representation of character encoded in UTF-8. This string is Null-terminated.
+     * Will be empty string if CaptionCharType is kDRCS because alternative codepoint is unknown.
+     */
     char u8str[8] = {0};
 public:
     CaptionChar() = default;
 
+    /**
+     * Helper function for calculating the width of the character block
+     */
     [[nodiscard]]
     int section_width() const {
         return (int)std::floor((float)(char_width + char_horizontal_spacing) * char_horizontal_scale);
     }
 
+    /**
+     * Helper function for calculating the height of the character block
+     */
     [[nodiscard]]
     int section_height() const {
         return (int)std::floor((float)(char_height + char_vertical_spacing) * char_vertical_scale);
     }
 };
 
+/**
+ * Structure contains DRCS data and related information.
+ */
 struct DRCS {
     int width = 0;
     int height = 0;
     int depth = 0;
     int depth_bits = 0;
     std::vector<uint8_t> pixels;
-    std::string md5;
-    std::string alternative_text;
-    uint32_t alternative_ucs4 = 0;
+    std::string md5;                ///< MD5 digest for the pixels buffer
+    std::string alternative_text;   ///< Appears only if alternative Unicode codepoint exists
+    uint32_t alternative_ucs4 = 0;  ///< Will be non-zero only if alternative Unicode codepoint exists
 public:
     DRCS() = default;
     DRCS(const DRCS&) = default;
@@ -130,13 +194,16 @@ public:
     DRCS& operator=(DRCS&&) noexcept = default;
 };
 
+/**
+ * Structure represents a caption region.
+ */
 struct CaptionRegion {
     std::vector<CaptionChar> chars;
     int x = 0;
     int y = 0;
     int width = 0;
     int height = 0;
-    bool is_ruby = false;
+    bool is_ruby = false;           ///< Will be true if the region is likely to be ruby text (furigana)
 public:
     CaptionRegion() = default;
     CaptionRegion(const CaptionRegion&) = default;
@@ -145,30 +212,80 @@ public:
     CaptionRegion& operator=(CaptionRegion&&) = default;
 };
 
+/**
+ * Constants for flags contained in Caption.
+ */
 enum CaptionFlags : uint8_t {
     kCaptionFlagsDefault = 0,
-    kCaptionFlagsClearScreen = 1u << 0,
-    kCaptionFlagsWaitDuration =  1u << 1
+    kCaptionFlagsClearScreen = 1u << 0,     ///< Screen should be cleared before the caption presentation
+    kCaptionFlagsWaitDuration =  1u << 1    ///< The caption has a determined duration
 };
 
+/**
+ * Structure represents a caption.
+ */
 struct Caption {
     CaptionType type = CaptionType::kDefault;
     CaptionFlags flags = CaptionFlags::kCaptionFlagsDefault;
 
-    // ISO 639-2 3-char language code, in Big Endian
-    // e.g. "jpn" => 6A 70 6E => 0x006A706E
+    /**
+     * ISO 639-2 3-char language code
+     * e.g. "jpn" => 6A 70 6E => 0x006A706E
+     */
     uint32_t iso6392_language_code = 0;
 
+    /**
+     * Caption statements represented in UTF-8 string.
+     * Ruby text is excluded in this string.
+     *
+     * Pay attention to the UTF-8 encoding if you are under Windows.
+     */
     std::string text;
+
+    /**
+     * Caption region array
+     */
     std::vector<CaptionRegion> regions;
+
+    /**
+     * A hashmap that contains all DRCS characters transmitted in current caption.
+     *
+     * Use CaptionChar::drcs_code as key for retrieving DRCS.
+     */
     std::unordered_map<uint32_t, DRCS> drcs_map;
 
+    /**
+     * Caption't presentation timestamp, in milliseconds
+     *
+     * Will be PTS_NOPTS if passed as PTS_NOPTS into decoder, otherwise in milliseconds.
+     */
     int64_t pts = 0;
+
+    /**
+     * Caption's duration, in milliseconds
+     *
+     * Will be DURATION_INDEFINITE if undetermined, otherwise in milliseconds.
+     */
     int64_t wait_duration = 0;
+
+    /**
+     * Width of the original(logical) caption plane. Usually has a value of 960 or 720.
+     */
     int plane_width = 0;
+
+    /**
+     * Height of the original(logical) caption plane. Usually has a value of 540 or 480.
+     */
     int plane_height = 0;
 
+    /**
+     * Represents whether the caption indicates a Built-in Sound Replay.
+     */
     bool has_builtin_sound = false;
+
+    /**
+     * The ID of build-in sound for playback. Valid only if has_builtin_sound is true.
+     */
     uint8_t builtin_sound_id = 0;
 public:
     Caption() = default;

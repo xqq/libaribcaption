b2e96f6bba621f88b179028d3ecf42ae81b4b751
xqq <xqq@xqq.im>
2022-01-30 21:04:11 +0900

    aribcaption/renderer: Add documents into public headers

--- include/aribcaption/renderer.h
+++ include/aribcaption/renderer.h
@@ -32,51 +32,114 @@
 extern "C" {
 #endif
 
+/**
+ * Enums for FontProvider indication
+ */
 typedef enum aribcc_fontprovider_type_t {
+    /**
+     * Detect and select FontProvider automatically. Should be used in most cases.
+     */
     ARIBCC_FONTPROVIDER_TYPE_AUTO = 0,
+
 #if defined(ARIBCC_USE_CORETEXT)
+    /**
+     * FontProvider relies on Apple CoreText API. Available on macOS and iOS.
+     */
     ARIBCC_FONTPROVIDER_TYPE_CORETEXT = 1,
 #endif
 
 #if defined(ARIBCC_USE_DIRECTWRITE)
+    /**
+     * FontProvider relies on DirectWrite API. Available on Windows 7+.
+     */
     ARIBCC_FONTPROVIDER_TYPE_DIRECTWRITE = 2,
 #endif
 
 #if defined(ARIBCC_USE_FONTCONFIG)
+    /**
+     * FontProvider using libfontconfig. Usually been used on Linux platforms.
+     */
     ARIBCC_FONTPROVIDER_TYPE_FONTCONFIG = 3,
 #endif
 
 #if defined(ARIBCC_IS_ANDROID)
+    /**
+     * FontProvider for Android. Available on Android 2.x+.
+     */
     ARIBCC_FONTPROVIDER_TYPE_ANDROID = 4,
 #endif
 
 #if defined(ARIBCC_USE_GDI_FONT)
+    /**
+     * FontProvder based on Win32 GDI API. Available on Windows 2000+.
+     */
     ARIBCC_FONTPROVIDER_TYPE_GDI = 5
 #endif
 } aribcc_fontprovider_type_t;
 
+/**
+ * Enums for TextRenderer indication
+ */
 typedef enum aribcc_textrenderer_type_t {
+    /**
+     * Detect and select TextRenderer automatically. Should be used in most cases.
+     */
     ARIBCC_TEXTRENDERER_TYPE_AUTO = 0,
+
 #if defined(ARIBCC_USE_CORETEXT)
+    /**
+     * Apple CoreText API based TextRenderer. Available on macOS and iOS.
+     */
     ARIBCC_TEXTRENDERER_TYPE_CORETEXT = 1,
 #endif
 
 #if defined(ARIBCC_USE_DIRECTWRITE)
+    /**
+     * DirectWrite API based TextRenderer. Available on Windows 7+.
+     */
     ARIBCC_TEXTRENDERER_TYPE_DIRECTWRITE = 2,
 #endif
 
 #if defined(ARIBCC_USE_FREETYPE)
+    /**
+     * Freetype based TextRenderer. Available on all platforms.
+     */
     ARIBCC_TEXTRENDERER_TYPE_FREETYPE = 3,
 #endif
 } aribcc_textrenderer_type_t;
 
+/**
+ * Enums for Renderer's caption storage policy indication
+ */
 typedef enum aribcc_caption_storage_policy_t {
+    /**
+     * The renderer will only keep minimal amount of captions for subsequent rendering if necessary.
+     * This is the default behavior.
+     */
     ARIBCC_CAPTION_STORAGE_POLICY_MINIMUM = 0,
+
+    /**
+     * The renderer will never evict appended captions unless you call the @aribcc_renderer_flush().
+     * This may result in memory waste. Use at your own risk.
+     */
     ARIBCC_CAPTION_STORAGE_POLICY_UNLIMITED = 1,
+
+    /**
+     * The renderer will keep appended captions at an upper limit of count.
+     */
     ARIBCC_CAPTION_STORAGE_POLICY_UPPER_LIMIT_COUNT = 2,
+
+    /**
+     * The renderer will keep appended captions at an upper limit of duration, in milliseconds.
+     */
     ARIBCC_CAPTION_STORAGE_POLICY_UPPER_LIMIT_DURATION = 3,
 } aribcc_caption_storage_policy_t;
 
+/**
+ * Enums for reporting rendering status
+ *
+ * See @aribcc_renderer_render()
+ */
 typedef enum aribcc_render_status_t {
     ARIBCC_RENDER_STATUS_ERROR = 0,
     ARIBCC_RENDER_STATUS_NO_IMAGE = 1,
@@ -84,64 +147,221 @@ typedef enum aribcc_render_status_t {
     ARIBCC_RENDER_STATUS_GOT_IMAGE_UNCHANGED = 3,
 } aribcc_render_status_t;
 
+/**
+ * Structure for holding rendered caption images
+ *
+ * See @aribcc_renderer_render()
+ */
 typedef struct aribcc_render_result_t {
-    int64_t pts;
-    int64_t duration;
+    int64_t pts;             ///< PTS of rendered caption
+    int64_t duration;        ///< duration of rendered caption, may be ARIBCC_DURATION_INDEFINITE
 
+    /**
+     * Rendered images array, may be NULL if error occurred or ARIBCC_RENDER_STATUS_NO_IMAGE returned.
+     *
+     * Do not manually free this array if the render result is received from the aribcc_renderer_t,
+     * instead, call @aribcc_render_result_cleanup().
+     */
     aribcc_image_t* images;
-    uint32_t image_count;
+    uint32_t image_count;    ///< element count of images array
 } aribcc_render_result_t;
 
+/**
+ * Cleanup the aribcc_render_result_t structure.
+ *
+ * Call this function only if if you received the render result from aribcc API.
+ * Otherwise it may cause a crash.
+ *
+ * This function doesn't release the memory of the @aribcc_render_result_t itself.
+ */
 ARIBCC_API void aribcc_render_result_cleanup(aribcc_render_result_t* render_result);
 
 
-// Opaque type
+/**
+ * ARIB STD-B24 caption renderer
+ *
+ * Opaque type
+ */
 typedef struct aribcc_renderer_t aribcc_renderer_t;
 
-
+/**
+ * A context is needed for allocating the Renderer.
+ *
+ * The context shouldn't be freed before any other object constructed from the context has been freed.
+ */
 ARIBCC_API aribcc_renderer_t* aribcc_renderer_alloc(aribcc_context_t* context);
 
+/**
+ * Free the renderer and all related resources
+ */
 ARIBCC_API void aribcc_renderer_free(aribcc_renderer_t* renderer);
 
+/**
+ * Initialize function must be called before calling any other member functions.
+ *
+ * @param renderer            @aribcc_renderer_t
+ * @param caption_type        Indicate caption type (kCaption / kSuperimpose)
+ * @param font_provider_type  Indicate @FontProviderType. Use kAuto in most cases.
+ * @param text_renderer_type  Indicate @TextRendererType. Use kAuto in most cases.
+ * @return true on success
+ */
 ARIBCC_API bool aribcc_renderer_initialize(aribcc_renderer_t* renderer,
                                            aribcc_captiontype_t caption_type,
                                            aribcc_fontprovider_type_t font_provider_type,
                                            aribcc_textrenderer_type_t text_renderer_type);
 
+/**
+ * Indicate stroke width for stroke text, in dots (relative)
+ *
+ * @param renderer  @aribcc_renderer_t
+ * @param dots      must >= 0.0f
+ */
 ARIBCC_API void aribcc_renderer_set_stroke_width(aribcc_renderer_t* renderer, float dots);
 
+/**
+ * Indicate whether render replaced DRCS characters as Unicode characters
+ *
+ * @param renderer  @aribcc_renderer_t
+ * @param replace   default as true
+ */
 ARIBCC_API void aribcc_renderer_set_replace_drcs(aribcc_renderer_t* renderer, bool replace);
 
+/**
+ * Indicate whether always render stroke text for all characters regardless of the indication by CharStyle
+ *
+ * @param renderer      @aribcc_renderer_t
+ * @param force_stroke  default as false
+ */
 ARIBCC_API void aribcc_renderer_set_force_stroke_text(aribcc_renderer_t* renderer, bool force_stroke);
 
+/**
+ * Indicate whether ignore rendering for ruby-like (furigana) characters
+ *
+ * @param renderer       @aribcc_renderer_t
+ * @param force_no_ruby  default as false
+ */
 ARIBCC_API void aribcc_renderer_set_force_no_ruby(aribcc_renderer_t* renderer, bool force_no_ruby);
 
+/**
+ * Indicate whether ignore background color rendering
+ *
+ * @param renderer             @aribcc_renderer_t
+ * @param force_no_background  default as false
+ */
 ARIBCC_API void aribcc_renderer_set_force_no_background(aribcc_renderer_t* renderer, bool force_no_background);
 
+/**
+ * Indicate font families (an array of font family names) for default usage
+ *
+ * Default font family will be used only if captions' language is unknown (iso6392_language_code == 0).
+ * Indicate force_default = true to force use these fonts always.
+ *
+ * The renderer contains an auto-fallback mechanism among indicated font families.
+ * The renderer also contains predefined font indications for Windows / macOS / Linux / Android.
+ *
+ * @param font_family    Array of font family names
+ * @param family_count   Element count of font_family array
+ * @param force_default  Whether force use these font families for all languages
+ * @return true on success
+ *
+ * @code
+ * const char* font_families[] = {
+ *     "Rounded M+ 1m for ARIB",
+ *     "Hiragino Maru Gothic ProN",
+ *     "sans-serif"
+ * }
+ * aribcc_renderer_set_default_font_family(renderer, font_families, 3, false);
+ * @endcode
+ */
 ARIBCC_API bool aribcc_renderer_set_default_font_family(aribcc_renderer_t* renderer,
                                                         const char** font_family,
                                                         size_t family_count,
                                                         bool force_default);
 
+/**
+ * Indicate font families (an array of font family names) for specific language
+ *
+ * The renderer contains an auto-fallback mechanism among indicated font families.
+ * The renderer also contains predefined font indications for Windows / macOS / Linux / Android.
+ *
+ * @param language_code  ISO639-2 Language Code, e.g. ARIBCC_MAKE_LANG('j', 'p', 'n')
+ * @param font_family    Array of font family names
+ * @param family_count   Element count of font_family array
+ * @return true on success
+ *
+ * @code
+ * const char* font_families[] = {
+ *     "Rounded M+ 1m for ARIB",
+ *     "Hiragino Maru Gothic ProN",
+ *     "sans-serif"
+ * }
+ * aribcc_renderer_set_language_specific_font_family(renderer, ARIBCC_MAKE_LANG('j', 'p', 'n'), font_families, 3);
+ * @endcode
+ */
 ARIBCC_API bool aribcc_renderer_set_language_specific_font_family(aribcc_renderer_t* renderer,
                                                                   uint32_t language_code,
                                                                   const char** font_family,
                                                                   size_t family_count);
 
+/**
+ * Set the renderer frame size in pixels, include margins. This function must be called before any render call.
+ *
+ * Usually rendered images will be inside this frame area, unless negative margin values are specified.
+ *
+ * @param renderer      @aribcc_renderer_t
+ * @param frame_width   must be >= 0
+ * @param frame_height  must be >= 0
+ * @return true on success
+ */
 ARIBCC_API bool aribcc_renderer_set_frame_size(aribcc_renderer_t* renderer, int frame_width, int frame_height);
 
+/**
+ * Set the frame margins in pixels. This function must be called after calling to @aribcc_renderer_set_frame_size().
+ * Call to this function is optional.
+ *
+ * Each value specifics the distance from the video rectangle to the renderer frame.
+ * Positive margin value means there will be free space between renderer frame and video area,
+ * while negative margin value lets renderer frame (visible area) inside the video, i.e. the video is cropped.
+ *
+ * If negative margin value has been indicated, rendered images may outside the renderer frame.
+ *
+ * @return true on success
+ */
 ARIBCC_API bool aribcc_renderer_set_margins(aribcc_renderer_t* renderer, int top, int bottom, int left, int right);
 
 ARIBCC_API void aribcc_renderer_set_storage_policy(aribcc_renderer_t* renderer,
                                                    aribcc_caption_storage_policy_t storage_policy,
                                                    size_t upper_limit);
 
+/**
+ * Append a caption into renderer's internal storage for subsequent rendering
+ *
+ * If a caption with same PTS already exists in the storage, it will be replaced by the new one.
+ *
+ * @param renderer  @aribcc_renderer_t
+ * @param caption   @aribcc_caption_t
+ * @return true on success
+ */
 ARIBCC_API bool aribcc_renderer_append_caption(aribcc_renderer_t* renderer, const aribcc_caption_t* caption);
 
+/**
+ * Render caption at specific PTS
+ *
+ * @param renderer    @aribcc_renderer_t
+ * @param pts         Presentation timestamp, in milliseconds
+ * @param out_result  Write back parameter for passing rendered images, images will be NULL if status is kError / kNoImage
+ * @return            ARIBCC_RENDER_STATUS_GOT_IMAGE / ARIBCC_RENDER_STATUS_GOT_IMAGE_UNCHANGED if rendered images provided
+ *                    ARIBCC_RENDER_STATUS_GOT_IMAGE_UNCHANGED means this batch of images is completely identical to the previous call
+ */
 ARIBCC_API aribcc_render_status_t aribcc_renderer_render(aribcc_renderer_t* renderer,
                                                          int64_t pts,
                                                          aribcc_render_result_t* out_result);
 
+/**
+ * Clear caption storage inside the renderer. Will evict all the appended captions.
+ *
+ * Call this function if the stream has been seeked, or met non-monotonic PTS.
+ */
 ARIBCC_API void aribcc_renderer_flush(aribcc_renderer_t* renderer);
 
 
--- include/aribcaption/renderer.hpp
+++ include/aribcaption/renderer.hpp
@@ -29,53 +29,116 @@
 
 namespace aribcaption {
 
+/**
+ * Enums for FontProvider indication
+ */
 enum class FontProviderType {
+    /**
+     * Detect and select FontProvider automatically. Should be used in most cases.
+     */
     kAuto = 0,
+
 #if defined(ARIBCC_USE_CORETEXT)
+    /**
+     * FontProvider relies on Apple CoreText API. Available on macOS and iOS.
+     */
     kCoreText = 1,
 #endif
 
 #if defined(ARIBCC_USE_DIRECTWRITE)
+    /**
+     * FontProvider relies on DirectWrite API. Available on Windows 7+.
+     */
     kDirectWrite = 2,
 #endif
 
 #if defined(ARIBCC_USE_FONTCONFIG)
+    /**
+     * FontProvider using libfontconfig. Usually been used on Linux platforms.
+     */
     kFontconfig = 3,
 #endif
 
 #if defined(ARIBCC_IS_ANDROID)
+    /**
+     * FontProvider for Android. Available on Android 2.x+.
+     */
     kAndroid = 4,
 #endif
 
 #if defined(ARIBCC_USE_GDI_FONT)
+    /**
+     * FontProvder based on Win32 GDI API. Available on Windows 2000+.
+     */
     kGDI = 5,
 #endif
 };
 
+/**
+ * Enums for TextRenderer indication
+ */
 enum class TextRendererType {
+    /**
+     * Detect and select TextRenderer automatically. Should be used in most cases.
+     */
     kAuto = 0,
+
 #if defined(ARIBCC_USE_CORETEXT)
+    /**
+     * Apple CoreText API based TextRenderer. Available on macOS and iOS.
+     */
     kCoreText = 1,
 #endif
 
 #if defined(ARIBCC_USE_DIRECTWRITE)
+    /**
+     * DirectWrite API based TextRenderer. Available on Windows 7+.
+     */
     kDirectWrite = 2,
 #endif
 
 #if defined(ARIBCC_USE_FREETYPE)
+    /**
+     * Freetype based TextRenderer. Available on all platforms.
+     */
     kFreetype = 3,
 #endif
 };
 
 namespace internal { class RendererImpl; }
 
+/**
+ * Enums for Renderer's caption storage policy indication
+ */
 enum class CaptionStoragePolicy {
+    /**
+     * The renderer will only keep minimal amount of captions for subsequent rendering if necessary.
+     * This is the default behavior.
+     */
     kMinimum = 0,
+
+    /**
+     * The renderer will never evict appended captions unless you call the @Renderer::Flush().
+     * This may result in memory waste. Use at your own risk.
+     */
     kUnlimited = 1,
+
+    /**
+     * The renderer will keep appended captions at an upper limit of count.
+     */
     kUpperLimitCount = 2,
+
+    /**
+     * The renderer will keep appended captions at an upper limit of duration, in milliseconds.
+     */
     kUpperLimitDuration = 3,
 };
 
+/**
+ * Enums for reporting rendering status
+ *
+ * See @Renderer::Render()
+ */
 enum class RenderStatus {
     kError = 0,
     kNoImage = 1,
@@ -83,40 +146,170 @@ enum class RenderStatus {
     kGotImageUnchanged = 3,
 };
 
+/**
+ * Structure for holding rendered caption images
+ */
 struct RenderResult {
-    int64_t pts = 0;
-    int64_t duration = 0;
+    int64_t pts = 0;             ///< PTS of rendered caption
+    int64_t duration = 0;        ///< duration of rendered caption, may be DURATION_INDEFINITE
     std::vector<Image> images;
 };
 
+/**
+ * ARIB STD-B24 caption renderer
+ */
 class Renderer {
 public:
+    /**
+     * A context is needed for constructing the Renderer.
+     *
+     * The context shouldn't be destructed before any other object constructed from the context has been destructed.
+     */
     ARIBCC_API explicit Renderer(Context& context);
     ARIBCC_API ~Renderer();
     ARIBCC_API Renderer(Renderer&&) noexcept;
     ARIBCC_API Renderer& operator=(Renderer&&) noexcept;
 public:
+    /**
+     * Initialize function must be called before calling any other member functions.
+     *
+     * @param caption_type        Indicate caption type (kCaption / kSuperimpose)
+     * @param font_provider_type  Indicate @FontProviderType. Use kAuto in most cases.
+     * @param text_renderer_type  Indicate @TextRendererType. Use kAuto in most cases.
+     * @return true on success
+     */
     ARIBCC_API bool Initialize(CaptionType caption_type = CaptionType::kCaption,
                                FontProviderType font_provider_type = FontProviderType::kAuto,
                                TextRendererType text_renderer_type = TextRendererType::kAuto);
 
+    /**
+     * Indicate stroke width for stroke text, in dots (relative)
+     * @param dots must >= 0.0f
+     */
     ARIBCC_API void SetStrokeWidth(float dots);
+
+    /**
+     * Indicate whether render replaced DRCS characters as Unicode characters
+     * @param replace default as true
+     */
     ARIBCC_API void SetReplaceDRCS(bool replace);
+
+    /**
+     * Indicate whether always render stroke text for all characters regardless of the indication by CharStyle
+     * @param force_stroke default as false
+     */
     ARIBCC_API void SetForceStrokeText(bool force_stroke);
+
+    /**
+     * Indicate whether ignore rendering for ruby-like (furigana) characters
+     * @param force_no_ruby default as false
+     */
     ARIBCC_API void SetForceNoRuby(bool force_no_ruby);
+
+    /**
+     * Indicate whether ignore background color rendering
+     * @param force_no_background default as false
+     */
     ARIBCC_API void SetForceNoBackground(bool force_no_background);
 
+    /**
+     * Indicate font families (an array of font family names) for default usage
+     *
+     * Default font family will be used only if captions' language is unknown (iso6392_language_code == 0).
+     * Indicate force_default = true to force use these fonts always.
+     *
+     * The renderer contains an auto-fallback mechanism among indicated font families.
+     * The renderer also contains predefined font indications for Windows / macOS / Linux / Android.
+     *
+     * @param font_family    Array of font family names
+     * @param force_default  Whether force use these font families for all languages
+     * @return true on success
+     */
     ARIBCC_API bool SetDefaultFontFamily(const std::vector<std::string>& font_family, bool force_default);
+
+    /**
+     * Indicate font families (an array of font family names) for specific language
+     *
+     * The renderer contains an auto-fallback mechanism among indicated font families.
+     * The renderer also contains predefined font indications for Windows / macOS / Linux / Android.
+     *
+     * @param language_code ISO639-2 Language Code, e.g. ThreeCC("jpn")
+     * @param font_family   Array of font family names
+     * @return true on success
+     */
     ARIBCC_API bool SetLanguageSpecificFontFamily(uint32_t language_code, const std::vector<std::string>& font_family);
+
+    /**
+     * Set the renderer frame size in pixels, include margins. This function must be called before any @Render() call.
+     *
+     * Usually rendered images will be inside this frame area, unless negative margin values are specified.
+     *
+     * @param frame_width   must be >= 0
+     * @param frame_height  must be >= 0
+     * @return true on success
+     */
     ARIBCC_API bool SetFrameSize(int frame_width, int frame_height);
+
+    /**
+     * Set the frame margins in pixels. This function must be called after calling to @SetFrameSize().
+     * Call to this function is optional.
+     *
+     * Each value specifics the distance from the video rectangle to the renderer frame.
+     * Positive margin value means there will be free space between renderer frame and video area,
+     * while negative margin value lets renderer frame (visible area) inside the video, i.e. the video is cropped.
+     *
+     * If negative margin value has been indicated, rendered images may outside the renderer frame.
+     *
+     * @return true on success
+     */
     ARIBCC_API bool SetMargins(int top, int bottom, int left, int right);
 
+    /**
+     * Set storage policy for renderer's internal caption storage
+     *
+     * @param policy       See @CaptionStoragePolicy
+     * @param upper_limit  Optional parameter, but must has a value for kUpperLimitCount & kUpperLimitDuration
+     */
     ARIBCC_API void SetStoragePolicy(CaptionStoragePolicy policy, std::optional<size_t> upper_limit = std::nullopt);
 
+    /**
+     * Append a caption into renderer's internal storage for subsequent rendering
+     *
+     * If a caption with same PTS already exists in the storage, it will be replaced by the new one.
+     *
+     * @param caption Caption's const reference
+     * @return true on success
+     */
     ARIBCC_API bool AppendCaption(const Caption& caption);
+
+    /**
+     * Append a caption into renderer's internal storage for subsequent rendering
+     *
+     * If a caption with same PTS already exists in the storage, it will be replaced by the new one.
+     *
+     * @param caption Caption's Rvalue reference, use std::move()
+     * @return true on success
+     */
     ARIBCC_API bool AppendCaption(Caption&& caption);
 
+    /**
+     * Render caption at specific PTS
+     *
+     * This function queries a caption from the specified PTS and render it into images if exists.
+     *
+     * @param pts         Presentation timestamp, in milliseconds
+     * @param out_result  Write back parameter for passing rendered images, will be empty if status is kError / kNoImage
+     *
+     * @return            kGotImage / kGotImageUnchanged if rendered images provided
+     *                    kGotImageUnchanged means this batch of images is completely identical to the previous call
+     */
     ARIBCC_API RenderStatus Render(int64_t pts, RenderResult& out_result);
+
+    /**
+     * Clear caption storage inside the renderer. Will evict all the appended captions.
+     *
+     * Call this function if the stream has been seeked, or met non-monotonic PTS.
+     */
     ARIBCC_API void Flush();
 public:
     Renderer(const Renderer&) = delete;
